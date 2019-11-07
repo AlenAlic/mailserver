7ac6d46b8d75b5cee41b4b573a39b83c2458c5c6
Alen Alic <aalic89@gmail.com>
2019-11-07 22:53:15 +0100

    token bug fix

--- src/api/auth.js
+++ src/api/auth.js
@@ -33,15 +33,17 @@ export class AuthenticatedUser {
    *
    */
   constructor(token) {
-    this.token = token;
-    const data = decode(token);
-    this.id = data.id;
-    this.email = data.email;
-    this.firstName = data.first_name;
-    this.lastName = data.last_name;
-    // Assumption: exp and iat fields are number of milliseconds since 1970
-    this.expiresAt = new Date(data.exp * 1000);
-    this.issuedAt = new Date(data.iat * 1000);
+    if (token != null) {
+      this.token = token;
+      const data = decode(token);
+      this.id = data.id;
+      this.email = data.email;
+      this.firstName = data.first_name;
+      this.lastName = data.last_name;
+      // Assumption: exp and iat fields are number of milliseconds since 1970
+      this.expiresAt = new Date(data.exp * 1000);
+      this.issuedAt = new Date(data.iat * 1000);
+    }
   }
 
   /**
